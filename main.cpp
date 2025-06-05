29f4c5b4075b300855845cb9d9004236b8124e72
Pablo Romero <75594450+RomeroPablo@users.noreply.github.com>
2025-06-05 01:44:05 -0500

    add slcan parser

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <string>
 #include <thread>
+#include <array>
 
 void serial_read(SerialPort &serial, RingBuffer &ringBuffer){
     std::vector<uint8_t> temp(READ_CHUNK);
@@ -29,6 +30,110 @@ void photon_proc(RingBuffer &ringBuffer){
         //replace this in prod, should write to app. buffer
         std::cout.write((char*)temp.data(), amount_read) << std::endl;
         std::cout.flush();
+    }
+}
+
+enum class ParseState : uint8_t {
+    WaitStart,
+    Id,
+    Len,
+    DataHigh,
+    DataLow,
+    End
+};
+
+static inline uint8_t hex_value(uint8_t c){
+    static const std::array<int8_t, 256> table = []{
+        std::array<int8_t, 256> t{};
+        t.fill(-1);
+        for(uint8_t i = 0; i < 10; ++i) t['0' + i] = i;
+        for(uint8_t i = 0; i < 6; ++i) {
+            t['A' + i] = 10 + i;
+            t['a' + i] = 10 + i;
+        }
+        return t;
+    }();
+    return table[c];
+}
+
+inline void dispatch(uint32_t id, uint8_t len, const uint8_t* payload){
+    //placeholder dispatch - replace with routing table
+    std::cout << "ID:" << std::hex << id << " LEN:" << std::dec
+              << static_cast<int>(len) << " DATA:";
+    for(uint8_t i = 0; i < len; ++i)
+        std::cout << std::hex << static_cast<int>(payload[i]);
+    std::cout << std::dec << std::endl;
+}
+
+void parse(const uint8_t* data, size_t len){
+    static ParseState state = ParseState::WaitStart;
+    static uint32_t id = 0;
+    static uint8_t id_digits = 0;
+    static uint8_t dlen = 0;
+    static uint8_t payload[8];
+    static uint8_t index = 0;
+
+    for(size_t i = 0; i < len; ++i){
+        uint8_t c = data[i];
+        switch(state){
+            case ParseState::WaitStart:
+                if(c == 't'){
+                    id = 0;
+                    id_digits = 0;
+                    state = ParseState::Id;
+                }
+                break;
+            case ParseState::Id: {
+                uint8_t v = hex_value(c);
+                if(v < 16){
+                    id = (id << 4) | v;
+                    if(++id_digits == 3)
+                        state = ParseState::Len;
+                }else{
+                    state = ParseState::WaitStart;
+                }
+                break;
+            }
+            case ParseState::Len: {
+                uint8_t v = hex_value(c);
+                if(v < 16){
+                    dlen = v;
+                    index = 0;
+                    state = dlen ? ParseState::DataHigh : ParseState::End;
+                }else{
+                    state = ParseState::WaitStart;
+                }
+                break;
+            }
+            case ParseState::DataHigh: {
+                uint8_t v = hex_value(c);
+                if(v < 16){
+                    payload[index] = v << 4;
+                    state = ParseState::DataLow;
+                }else{
+                    state = ParseState::WaitStart;
+                }
+                break;
+            }
+            case ParseState::DataLow: {
+                uint8_t v = hex_value(c);
+                if(v < 16){
+                    payload[index] |= v;
+                    if(++index == dlen)
+                        state = ParseState::End;
+                    else
+                        state = ParseState::DataHigh;
+                }else{
+                    state = ParseState::WaitStart;
+                }
+                break;
+            }
+            case ParseState::End:
+                if(c == '\r')
+                    dispatch(id, dlen, payload);
+                state = ParseState::WaitStart;
+                break;
+        }
     }
 }
 
